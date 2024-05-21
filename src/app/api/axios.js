dc2284b998a916e9f1e54fad48070a796af7b6c6
TENSAEA <tensaeaschalew26@gmail.com>
2024-05-21 11:24:19 +0300

    update backend url

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://quicknotes-api.onrender.com",
+  baseURL: "https://quicknotes-api.onrender.com/api",
 });
 
 // Request interceptor for adding auth token
